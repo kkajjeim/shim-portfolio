c5c90fef3abf8222569551820c911ac07df9d25e
Kyungju SHIM <roundshoulderim@gmail.com>
2019-12-20 01:09:18 +0900

    ADD parallax effect on Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hello kyungju</p>
+        <Main />
       </header>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => (
+  <div
+    style={{
+      position: "fixed",
+      height: "50px",
+      backgroundColor: "white",
+      width: "100%",
+      fontFamily: "Raleway, sans-serif"
+    }}
+  >
+    <div style={{ marginTop: "1%" }}>KYUNG JU SHIM</div>
+  </div>
+);
+
+export default Nav;
